2f49ac02963ad9f7c0437859d0c4f995d7809756
dongdong <dongdong@eyesvision.com>
2021-06-07 16:10:24 +0900

    리덕스도 되었고 이제 api연동해야함(1020)

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -59,23 +59,27 @@ const textMap = {
   register: '회원가입',
 };
 
-const AuthForm = ({ type }) => {
+const AuthForm = ({ type, form, onChange, onSubmit }) => {
   const text = textMap[type];
 
   return (
     <AuthFormBlock>
       <h3>{text}</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <StyledInput
           autoComplete="username"
           name="username"
           placeholder="아이디"
+          onChange={onChange}
+          value={form.username}
         />
         <StyledInput
           autoComplete="new-password"
           name="password"
           placeholder="비밀번호"
           type="password"
+          onChange={onChange}
+          value={form.password}
         />
         {type === 'register' && (
           <StyledInput
@@ -83,9 +87,11 @@ const AuthForm = ({ type }) => {
             name="passwordConfirm"
             placeholder="비밀번호 확인"
             type="password"
+            onChange={onChange}
+            value={form.passwordConfirm}
           />  
         )}
-        <ButtonWithMarginTop cyan fullWidth>로그인</ButtonWithMarginTop>
+        <ButtonWithMarginTop cyan fullWidth>{ text }</ButtonWithMarginTop>
       </form>
       <Footer>
         {type === 'login' ? (
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,11 +1,19 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
 
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
-
-export const sampleAction = createAction(SAMPLE_ACTION);
-
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({ form, key, value }) => ({
+    form, // register , login
+    key, // username, password, passwordConfirm
+    value // 실제 바꾸려는 값
+  })
+);
+export const initializeForm = createAction(INITIALIZE_FORM, form => form); // register / login
 
 const initialState = {
   register: {
@@ -17,13 +25,18 @@ const initialState = {
     username: '',
     password: ''
   },
-  auth: null,
-  authError: null
 };
 
 const auth = handleActions(
   {
-    [SAMPLE_ACTION]: (state, action) => state,
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, draft => {
+        draft[form][key] = value; // 예: state.register.username을 바꾼다
+      }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+    }),
   },
   initialState
 );
